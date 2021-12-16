3c11f274776fd30e7541553c902f19f79987612c
Gurb-S <OrganicMintz@gmail.com>
2021-12-16 02:19:43 +0100

    add colors availability for chosen design

--- js/script.js
+++ js/script.js
@@ -16,3 +16,40 @@ jobRole.addEventListener('click',(e)=>{
         otherJob.style.display = 'none';
     }
 });
+
+const color = document.getElementById('color');
+//color.style.backgroundColor = 'purple';
+color.disabled = true;
+
+const designs = document.getElementById('design');
+designs.addEventListener('click',(e)=>{
+    const designChoice = e.target.value;
+    console.log(e.target);
+    const options = color.children;
+    color.value = 'Select a design theme above';
+    if(designChoice === 'js puns'){
+        for(let i = 1;i < 7; i++){
+            if(i <= 3){
+                options[i].style.display = '';
+                color.disabled = false;
+            }
+            else if(i >= 4){
+                options[i].style.display = 'none';
+                color.disabled = false;
+            }   
+        }
+    }
+    else if(designChoice === 'heart js'){
+        
+        for(let i = 1;i < 7; i++){
+            if(i <= 3){
+                options[i].style.display = 'none';
+                color.disabled = false;
+            }
+            else if(i >= 4){
+                options[i].style.display = '';
+                color.disabled = false;
+            }   
+        }
+    }
+});
