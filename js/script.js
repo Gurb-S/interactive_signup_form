593f07b228b838623354fde3ec626866dac5d6bf
Gurb-S <OrganicMintz@gmail.com>
2021-12-16 01:10:46 +0100

    add text box when 'Other' is selected as job role

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-alert('Booyahh');
+const nameField = document.getElementById('name');
+nameField.focus();
+
+const otherJob = document.getElementById('other-job-role');
+otherJob.style.display = 'none';
+
+const jobRole = document.getElementById('title');
+
+jobRole.addEventListener('click',(e)=>{
+    const roleSelected = e.target.value;
+    console.log(roleSelected);
+    if(roleSelected === 'other'){
+        otherJob.style.display = '';
+    }
+    else{
+        otherJob.style.display = 'none';
+    }
+});
